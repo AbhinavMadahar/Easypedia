b4747bc16d75f098ce07c8c974f69d3b285343e9
Abhinav Madahar <veenamadahar@gmail.com>
2015-06-03 09:05:57 +0000

    On a disambiguation, use the first link

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 var wtf_wikipedia = require("wtf_wikipedia");
 
 var easypedia = function(pageName, next) {
-    wtf_wikipedia.from_api(pageName, "en", function(markup) {
-        next(wtf_wikipedia.parse(markup));
-    });
+	wtf_wikipedia.from_api(pageName, "en", function(page) {
+		var parsed = wtf_wikipedia.parse(page);
+
+		if (parsed.type === "disambiguation")
+			easypedia(parsed.pages[0], next);
+		else
+			next(parsed);
+
+	});
 };
 
 module.exports = easypedia;
